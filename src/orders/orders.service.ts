d525a5d5a14f79077779eb09c145ebe287302c2a
jeffer <126474495+condiza@users.noreply.github.com>
2025-03-19 17:12:27 -0500

    Configuration-Orders

---
+++ src/orders/orders.service.ts
@@ -0,0 +1,36 @@
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { PrismaClient } from '@prisma/client';
+import { RpcException } from '@nestjs/microservices';
+
+@Injectable()
+export class OrdersService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('OrdersServices');
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Data base connected');
+  }
+  create(createOrderDto: CreateOrderDto) {
+    return this.order.create({
+      data: createOrderDto,
+    });
+  }
+
+  findAll() {
+    return this.order.findMany();
+  }
+
+  async findOne(id: string) {
+    const order = await this.order.findFirst({
+      where: { id },
+    });
+    if (!order) {
+      throw new RpcException({
+        message: `Order with id #${id} not found`,
+        status: HttpStatus.NOT_FOUND,
+      });
+    }
+    return order;
+  }
+}
